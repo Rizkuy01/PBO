a19dbaf4faa7c3b01d73438bdfe406de14ed1ce9
Rizkuy01 <rizkysaputraa2004.com>
2024-02-19 17:24:03 +0700

    update week3

--- week1/mobil.java
+++ week1/mobil.java
@@ -2,5 +2,10 @@ package week1;
 
 public class mobil {
     String warna;
+    String jenis;
     int tahunProduksi;
+
+    void kondisiMesin(boolean kondisi) {
+        System.out.println("Kondisi Mesin: " + kondisi);
+    }
 }
--- week1/mobilBeraksi.java
+++ week1/mobilBeraksi.java
@@ -2,17 +2,30 @@ package week1;
 
 public class mobilBeraksi {
     public static void main(String[] args) {
+
+        // Object1
         mobil mobilku = new mobil();
         mobilku.warna = "Hitam";
         mobilku.tahunProduksi = 2024;
 
+        // Object2
         mobil mobilmu = new mobil();
         mobilmu.warna = "Maroon";
         mobilmu.tahunProduksi = 2021;
 
+        // Object3
+        mobil mobilPanji = new mobil();
+        mobilPanji.warna = "biru";
+        mobilPanji.jenis = "amphibi";
+
+        // Output
         System.out.println("Rektor mempunyai pajero " + mobilku.warna + " dan Civic rektor berwarna " + mobilmu.warna);
         System.out.println("Dikirim dari tahun produksi " + mobilmu.tahunProduksi + " dan pajaknya mati ditahun "
                 + mobilku.tahunProduksi);
+        System.out.println("Mobil Panji itu mobil " + mobilPanji.jenis + " berwarna " + mobilPanji.warna);
+
+        boolean mesin = true;
+        mobilku.kondisiMesin(mesin);
     }
 
 }
